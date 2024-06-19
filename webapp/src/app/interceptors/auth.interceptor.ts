5f7c0a3e74f42294e843100d131d01603100c917
Sandro Lain <sandrolain@outlook.com>
2024-06-19 06:41:54 +0200

    [feat] session expired interceptor

--- webapp/src/app/interceptors/auth.interceptor.ts
+++ webapp/src/app/interceptors/auth.interceptor.ts
@@ -1,13 +1,48 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {
+  HttpErrorResponse,
+  HttpHandlerFn,
+  HttpInterceptorFn,
+  HttpRequest,
+} from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 import { AuthService } from '../services/auth/auth.service';
+import { NotificationService } from '../services/notification/notification.service';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const authToken = inject(AuthService).getAuthToken();
+export const authInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const notificationService = inject(NotificationService);
+
+  const authToken = authService.getAuthToken();
   if (authToken) {
     req = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${authToken}`),
     });
   }
-  return next(req);
+
+  return next(req).pipe(
+    catchError((err) => {
+      if (err instanceof HttpErrorResponse) {
+        // Handle HTTP errors
+        if (err.status === 401) {
+          authService.logout();
+          router.navigate(['/']);
+          notificationService.error(
+            $localize`Session expired. Please login again.`
+          );
+        } else {
+          console.error('HTTP error:', err);
+        }
+      } else {
+        console.error('An error occurred:', err);
+      }
+      // Re-throw the error to propagate it further
+      return throwError(() => err);
+    })
+  );
 };
